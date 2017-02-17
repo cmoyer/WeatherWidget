a9aadf79015a647c3a28b85e601b9b30b0f7e729
Chad Moyer <chad.moyer@trumbullcorp.com>
2017-02-17 14:08:03 -0500

    set up a skeleton template of what our weather component will look like.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+        <div class="container">
+            <div class="col-xs-4">
+                <weather-widget></weather-widget>
+            </div>
+        </div>
+    `,
+    styles: [ `
+        .container {
+            padding-top: 5rem;
+        }
+    `]
 })
 export class AppComponent  { name = 'Angular'; }
