937ac234771b9dcb440ed22eede092beeeabd4a4
Chad Moyer <chad.moyer@trumbullcorp.com>
2017-02-20 10:58:14 -0500

    updated gitignore to ignore our constants file containing our api keys.

--- src/app/weather-widget/service/weather.service.ts
+++ src/app/weather-widget/service/weather.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { FORECAST_KEY, FORECAST_ROOT } from '../constants/constants';
+
 @Injectable()
 export class WeatherService {
-
+    getCurrentLocation(): [number, number] {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(pos => {
+                console.log("Position: ", pos.coords.latitude, ",", pos.coords.longitude); // TODO: REMOVE
+                return [pos.coords.latitude, pos.coords.longitude];
+            },
+            err => console.error("Unable to get the position - ", err));
+        } else {
+            console.error("Geolocation is not available");
+            return [0,0];
+        }
+    }
 }
